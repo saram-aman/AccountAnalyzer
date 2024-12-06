305c41263fe8ece9967248250027d3c6d32bf010
Aman Chaudhary <aman.chaudhary@bizsupportc.com>
2024-12-06 12:34:54 +0530

    setup process!

---
+++ App/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component<any, any> {
+    render(): React.ReactNode {
+        return (
+            <div className='justify-items-center'>
+                <div className='text-base font-bold'>Welcome to AccountAnalyzer!</div>
+            </div>
+        )
+    }
+}
